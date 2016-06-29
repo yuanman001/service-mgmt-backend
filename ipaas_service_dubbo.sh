6e3e95b131ea856755d90baf96ccad9c75520ec8
yuanman <yuanman@asiainfo.com>
2016-06-29 18:16:36 +0800

    update start.sh

--- ipaas_service_dubbo.sh
+++ ipaas_service_dubbo.sh
@@ -13,13 +13,13 @@ export CLASSPATH
 MEM_ARGS="-Xms256m -Xmx512m -XX:PermSize=64M -XX:MaxPermSize=128M"
 JAVA_OPTIONS="-Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true -Dsun.net.inetaddr.ttl=10 -Dpaas.dubbo.provider.timeout=300000"
 
-START_CMD="${MEM_ARGS} -Dpaas.dubbo.registry.address=${DUBBO_REGISTRY_ADD} -Dpaas.dubbo.protocol.port=${DUBBO_PORT} ${JAVA_OPTIONS} com.ai.paas.ipaas.DubboServiceStart > /ipaas-service-dubbo-${DUBBO_PORT}.log & 2 > 1 &"
+START_CMD="${MEM_ARGS} -Dpaas.dubbo.registry.address=${DUBBO_REGISTRY_ADDR} -Dpaas.dubbo.protocol.port=${DUBBO_PORT} ${JAVA_OPTIONS} com.ai.paas.ipaas.DubboServiceStart > /ipaas-service-dubbo-${DUBBO_PORT}.log & 2 > 1 &"
 
 echo ${JAVA_HOME}
 echo ${CLASSPATH}
 echo ${DUBBO_PORT}
 echo ${START_CMD}
 
-echo "------------------- portal dubbo start --------------------"
+echo "------------------- ipaas service dubbo start --------------------"
 java ${START_CMD}
-echo "------------------- portal dubbo server started! -------------------"
+echo "------------------- ipaas service dubbo started! -------------------"
