8e87a8c853d95c28d0c2b7d00ea831e881992171
WTF <1031248990@qq.com>
2016-08-08 16:13:43 +0800

    changed transfer pojo int -> String

--- iPaaS-RDS-X/src/main/java/com/ai/paas/ipaas/rds/service/transfer/vo/InstanceBaseSimple.java
+++ iPaaS-RDS-X/src/main/java/com/ai/paas/ipaas/rds/service/transfer/vo/InstanceBaseSimple.java
@@ -3,24 +3,24 @@ package com.ai.paas.ipaas.rds.service.transfer.vo;
 import com.ai.paas.ipaas.rds.dao.mapper.bo.RdsIncBase;
 
 public class InstanceBaseSimple {
-	private Integer id;
+	private String id;
     private String userId;
     private String serviceId;
     private String incName;
-    private Integer incType;
+    private String incType;
     private String incIp;
-    private Integer incPort;
+    private String incPort;
     private String rootName;
     private String rootPassword;
     
     public InstanceBaseSimple(RdsIncBase incBase) {
-    	this.id = incBase.getId();
+    	this.id = incBase.getId() + "";
     	this.userId = incBase.getUserId();
     	this.serviceId = incBase.getServiceId();
     	this.incName = incBase.getIncName();
-    	this.incType = incBase.getIncType();
+    	this.incType = incBase.getIncType() + "";
     	this.incIp = incBase.getIncIp();
-    	this.incPort = incBase.getIncPort();
+    	this.incPort = incBase.getIncPort() + "";
     	this.rootName = incBase.getRootName();
     	this.rootPassword = incBase.getRootPassword();
 	}
@@ -30,13 +30,13 @@ public class InstanceBaseSimple {
 	}
 
 	public void setByIncBase(RdsIncBase incBase){
-    	this.id = incBase.getId();
+    	this.id = incBase.getId() + "";
     	this.userId = incBase.getUserId();
     	this.serviceId = incBase.getServiceId();
     	this.incName = incBase.getIncName();
-    	this.incType = incBase.getIncType();
+    	this.incType = incBase.getIncType() + "";
     	this.incIp = incBase.getIncIp();
-    	this.incPort = incBase.getIncPort();
+    	this.incPort = incBase.getIncPort() + "";
     	this.rootName = incBase.getRootName();
     	this.rootPassword = incBase.getRootPassword();
     }
