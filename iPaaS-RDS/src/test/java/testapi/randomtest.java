07fa49e9555bd452747050f0f9e117852a329e00
houbin <1031248990@qq.com>
2016-10-28 15:57:24 +0800

    add lose build.gradle ...

---
+++ iPaaS-RDS/src/test/java/testapi/randomtest.java
@@ -0,0 +1,46 @@
+package testapi;
+
+import java.util.Random;
+
+/** 
+ * @author  作者 “WTF” E-mail: 1031248990@qq.com
+ * @date 创建时间：2016年9月26日 下午4:03:57 
+ * @version 
+ * @since  
+ */
+public class randomtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rand = new Random();
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()" + rand.nextInt());
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+		System.out.println("rand.nextInt()%cpus.size() : " + rand.nextInt()%2 );
+	}
+
+}
